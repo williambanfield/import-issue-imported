b3f5c4b7316ffb374b93198ed73fb63521a77d9f
William Banfield <wbanfield@gmail.com>
2021-12-11 15:09:52 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/williambanfield/import-issue-imported
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("hello from version 0.1")
+}
+
+func main() {
+}
